64706b47b75be28c09f260cf8adbd5a8c56e9580
phucphan2001 <phucphan0708@gmail.com>
2022-10-19 13:04:56 +0700

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('next').onclick = function(){
+    let list = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(list[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let list = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(list[list.length -1]);
+}
+setInterval(function(){
+    let list = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(list[0]);
+},8000)
